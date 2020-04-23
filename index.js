9d0a2f3882677150fed493452c55f9a85ca7bfdd
rubn-g <ruben.gonzalezgomez@gmail.com>
2020-04-23 21:54:27 +0200

    redis only included if used

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const redis = require('./src/emitter/redis');
 const node = require('./src/emitter/node');
 
 module.exports = (name, options = {}) => {
+	const redis = require('./src/emitter/redis');
+
 	options = Object.assign({}, options);
 	options.keyPrefix = (options.keyPrefix || 'karrier') + ':';
 	options.lazyConnect = true;
